bbac2dab8e0dad0b057309d3dd94e1ee78dba6bb
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-30 01:02:08 +0300

    решение задачи

--- final_homework/Program.cs
+++ final_homework/Program.cs
@@ -9,3 +9,52 @@
 ["Russia", "Denmark", "Kazan"] -> []
 */
 
+// Ввод элементов массива с консоли
+string[] InitArray()
+{
+   string text = string.Empty;
+   string[] array = new string[0];
+   while (text != "Q")
+   {
+      Console.WriteLine("Введите элемент. Конец ввода 'Q'");
+      text = Console.ReadLine()!;
+      if (text != "Q")
+      {
+         array = array.Append(text).ToArray();
+      }
+   }
+   return array;
+}
+
+// Получение массива элементов с не более 3 символов
+string[] GetArray(string[] initArray)
+{
+   string[] newArray = new string[0];
+   for (int i = 0; i < initArray.Length; i++)
+   {
+      if (initArray[i].Length <= 3)
+      {
+         newArray = newArray.Append(initArray[i]).ToArray();
+      }
+   }
+   return newArray;
+}
+
+// Печать массива
+void PrintArray(string[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+      Console.Write($"'{array[i]}', ");
+   }
+   Console.WriteLine("");
+}
+
+string[] initArray = InitArray();
+Console.WriteLine("\nВведеный массив:");
+PrintArray(initArray);
+
+string[] newArray = GetArray(initArray);
+Console.WriteLine("\nМассив с элементами не более 3 символов:");
+PrintArray(newArray);
+
